f66e6babf286422b134f0e23eec645ee6f205ff2
unknown <C00218004@itcarlow.ie>
2019-09-25 14:57:59 +0100

    added filters

---
+++ Assets/Plugins/RunningTotal/RunningTotalFilter.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+
+public class RunningTotalFilter
+{
+    public static List<int> runningTotal(List<int> inputNumbers)
+    {
+        List<int> result = new List<int>();
+        int runningTotal = 0;
+        foreach(int number in inputNumbers)
+        {
+            runningTotal += number;
+            result.Add(runningTotal);
+        }
+        return result;
+    }
+}
