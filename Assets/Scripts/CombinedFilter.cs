2c397078762086ce77764b59e3a6b909e4490ad4
unknown <C00218004@itcarlow.ie>
2019-09-27 11:01:53 +0100

    adding files

---
+++ Assets/Scripts/CombinedFilter.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class CombinedFilter
+{
+    public static int[] combinedFilter(int[] xs)
+    {
+        return RunningTotalFilter.runningTotal(Modulo3.modulo3(XOR21Filter.XOR21s(xs)).ToList<int>()).ToArray();
+    }
+}
---
+++ Assets/Tests/CombinedFilterTest.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class CombinedFilterTest
+    {
+        [Test]
+        public void CombinedFilterTestSimplePasses()
+        {
+            int[] inputArr = { 2 };
+            int[] outputArr = CombinedFilter.combinedFilter(inputArr);
+            int[] expected = { 2 };
+            CollectionAssert.AreEqual(expected, outputArr);
+        }
+    }
+}
